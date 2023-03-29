b7efd7589783ff506b961e2421ea55bea4078bf1
Ruk47 <129020483+Ruk47@users.noreply.github.com>
2023-03-29 09:57:45 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 // For command line git tutorial
-// Local repository modification
-
-=======
-// For command line git tutorial 
-// GitHub modification
->>>>>>> origin/master
+// First: GitHub modification
+// Second: Local repository modification
+  
 print("Hello World")
 print("Tell Your World")
 print("Tell His World")
